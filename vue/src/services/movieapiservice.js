ec2481d28d4c74cb0deffbfb4e9095ad5bea0bc9
Amanda Montalvo <amon424@wgu.edu>
2023-09-27 16:45:19 -0500

    new api link

--- vue/src/services/movieapiservice.js
+++ vue/src/services/movieapiservice.js
@@ -6,6 +6,6 @@ getTrending(){
     return axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=ea5f97c7d5398eac0c2b48ed86ab30dc')
 }, 
 getMoviesByGenre(genreId){
-    return axios.get('https://api.themoviedb.org/3/discover/movie?api_key=21acfcaef5e02aab042a22bddac8ebbb&with_genres=' + genreId)
+    return axios.get('https://api.themoviedb.org/3/discover/movie?api_key=6c954a0b1939a235aec10e67d5cc9b58&with_genres=' + genreId)
 }
 }
