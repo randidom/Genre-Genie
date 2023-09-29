ed75f15ab93dbd6c9603735472fa77a9c7dbaaef
dieynaba sy <dieynabasy16@gmail.com>
2023-09-29 11:53:23 -0400

    done

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,7 +4,6 @@
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       <router-link v-bind:to="{name: 'login'}" v-else>Login</router-link>
        <router-link v-bind:to="{ name: 'Recommendation' }" v-if="$store.state.token != ''">Recommendation</router-link>
-       <router-link v-bind:to="{ name: 'search' }" v-if="$store.state.token != ''">Search By Genre</router-link>
       <router-link v-bind:to="{ name: 'search' }" v-if="$store.state.token != ''">Search</router-link>
        <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp;
     </div>
--- vue/src/views/YourRecommendation.vue
+++ vue/src/views/YourRecommendation.vue
@@ -1,31 +1,40 @@
 <template>
-<div>
-  <div class="Title">
-       <h1>Add Movies to Favorites</h1>
-       </div>
- 
-  <div class="container">
-     
-   
-<div class="Three-columns">
-      <div class="column"><h3>Add favorite</h3><br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore laudantium sapiente quam unde illum rem<div class="button-container"><button class="button1">Add Favorite</button></div>
-       </div>
-       </div>
-    
-      
-
-    <div class="Three-columns1">
-      <div class="column1"><h3>Add favorite</h3><br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore laudantium sapiente quam unde illum rem<div class="button-container"><button class="button2">Add Favorite</button></div>
-     </div>
-      </div>
-
-    <div class="Three-columns2">
-      <div class="column2"><h3>Add favorite</h3><br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore laudantium sapiente quam unde illum rem<div class="button-container"><button class="button3">Add Favorite</button></div>
+  <div>
+    <div class="Title">
+      <h1> Movie Recommendations</h1>
     </div>
-     </div>
-
+    <div class="container">
+      <div class="Ultimate-grid">
+        <div class="item">
+          <div class="content">
+            <h3>Add favorite</h3>
+            <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore laudantium sapiente quam unde illum rem
+          </div>
+          <div class="button-container">
+            <button class="button1">Add Favorite</button>
+          </div>
+        </div>
+        <div class="item">
+          <div class="content">
+            <h3>Add favorite</h3>
+            <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore laudantium sapiente quam unde illum rem
+          </div>
+          <div class="button-container">
+            <button class="button2">Add Favorite</button>
+          </div>
+        </div>
+        <div class="item">
+          <div class="content">
+            <h3>Add favorite</h3>
+            <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore laudantium sapiente quam unde illum rem
+          </div>
+          <div class="button-container">
+            <button class="button3">Add Favorite</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -35,132 +44,77 @@ export default {
 </script>
 
 <style scoped>
-
-.Title{
-position: absolute;
-text-align: left;
-margin: 20px;
-left: 20px;
-color: white;
-font-size: 30px;
-}
 .container {
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  /* min-height: 85vh; */
   padding-bottom: 20px;
   margin: 30px;
   width: 100%;
   height: 850px;
- 
 }
-
-.Three-columns {
-  display: flex;
-  justify-content: space-between;
-  width: 85%;
-  padding:10px 0;
-  background-color: rgba( 22,29,117,0.5);
-  height: 70%;
-  align-items: flex-end;
+.Ultimate-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 60px;
+  max-width: 4000px;
+  padding: 20px;
+}
+.item {
+  background-color: rgba(22, 29, 117, 0.5);
   color: white;
-}
-
-.Three-columns1 {
-  display: flex;
-  justify-content: space-between;
-  width: 85%;
-  padding:10px 0;
-  background-color:rgba(12,238,238,0.5);
-  height: 70%;
-  align-items: flex-end;
-   color: white;
-}
-.button1{
-  background-color: #331d80;
-  width: 180px;
-    color: #000;
-    font-size: 12px;
-    padding:12px 0;
-    border: 0;
-    cursor: pointer;
-    border-radius: 9px;
-    outline: none;
-    margin-top: 30px;
-}
-.button2{
-  background-color:rgb(248, 163, 5); 
-  width: 180px;
-    color: #000;
-    font-size: 12px;
-    padding:12px 0;
-    border: 0;
-    cursor: pointer;
-    border-radius: 9px;
-    outline: none;
-    margin-top: 30px;
-}
-.button-container{
+  border-radius: 10px;
   text-align: center;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 30px;
-  height: 100%;
-}
-.button3{
-background-color: #331d80;
-  width: 180px;
-    color: #000;
-    font-size: 12px;
-    padding:12px 0;
-    border: 0;
-    border-radius: 9px;
-    outline: none;
-    cursor: pointer;
-    margin-top: 30px;
-     transition: background-color 0.3s ease;
-}
-.button1:hover {
-  background-color: #0056b3;
-}
-.button2:hover {
-  background-color: rgb(247, 194, 97); ;
-}
-.button3:hover {
-  background-color: #0056b3;
-}
-
-.Three-columns2 {
-  display: flex;
+  flex-direction: column;
   justify-content: space-between;
-  width: 89%;
-  padding:10px 0;
-  background-color: rgba( 22,29,117,0.5);
-  height: 70%;
-  align-items: flex-end;
+  height: 100%; 
+}
+.content {
+  padding: 20px;
+}
+.button-container {
+  margin-top: 20px;
+}
+.button1 {
+  background-color: #331D80;
+  width: 180px;
+  color: #000;
+  font-size: 12px;
+  padding: 12px 0;
+  border: 0;
+  cursor: pointer;
+  border-radius: 9px;
+  outline: none;
+}
+.button2 {
+  background-color: rgb(248, 163, 5);
+  width: 180px;
+  color: #000;
+  font-size: 12px;
+  padding: 12px 0;
+  border: 0;
+  cursor: pointer;
+  border-radius: 9px;
+  outline: none;
+}
+.button3 {
+  background-color: #331D80; 
+  width: 180px;
+  color: #000;
+  font-size: 12px;
+  padding: 12px 0;
+  border: 0;
+  border-radius: 9px;
+  outline: none;
+  cursor: pointer;
+}
+.Title {
+  position: absolute;
+  text-align: left;
+  margin: 20px;
+  left: 20px;
   color: white;
+  font-size: 30px;
 }
-
-.column {
-  padding: 20px;
-  
-  text-align: center;
-  min-height: 180px; 
-}
-
-.column1 {
-  padding: 20px;
- 
-  text-align: center;
-  min-height: 180px; 
-}
-.column2 {
-  padding: 20px;
-  
-  text-align: center;
-  min-height: 180px; 
-}
-
 </style>
