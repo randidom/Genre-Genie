780c5e140b1da2f720e6e31b18c8c0195986ef3e
Randi Dominick <randidom@Gmail.com>
2023-10-04 10:21:48 -0400

    Fixed small spelling

--- vue/src/views/YourRecommendation.vue
+++ vue/src/views/YourRecommendation.vue
@@ -14,7 +14,7 @@
           </div>
           <div class="button-container">
             <button class="button1" style="margin-top: 10px;" @click="addToFavorites(index)" :class="{ 'remove-favorite': selectedMovies.includes(movie.id) }">
-            {{ selectedMovies.includes(movie.id) ? 'Remove From Favorites' : 'Add to Favorites' }}
+            {{ selectedMovies.includes(movie.id) ? 'Remove from Favorites' : 'Add to Favorites' }}
             </button>
           </div>
         </div>
